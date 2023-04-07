d003a45571dc9b3dcd3bd71c2d8643860cb6f2ba
princekr96 <princekr700@gmail.com>
2023-04-08 00:25:30 +0530

    theme added using system preferences

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
+  const colorScheme = useColorScheme();
+  console.log(colorScheme);
+
+  const themeTextStyle =
+    colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+  const themeBgStyle =
+    colorScheme === 'light' ? styles.lightBgContainer : styles.darkBgContainer;
+
   return (
     <SafeAreaProvider>
-      <View style={styles.container}>
-        <Text>Stopwatch</Text>
+      <View style={[styles.container, themeBgStyle]}>
+        <Text style={[styles.heading, themeTextStyle]}>Stopwatch</Text>
         <StatusBar style="auto" />
       </View>
     </SafeAreaProvider>
@@ -19,5 +27,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  heading: {
+    fontSize: '30px',
+  },
+  lightThemeText: {
+    color: '#242c40',
+  },
+  darkThemeText: {
+    color: '#fff',
+  },
+  lightBgContainer: {
+    backgroundColor: '#d0d0c0',
+  },
+  darkBgContainer: {
+    backgroundColor: '#242c40',
   },
 });
