fe64166ee6e4aeb726e7baa66ebb39b776fb1a58
princekr96 <princekr700@gmail.com>
2023-04-16 18:39:29 +0530

    initial function written for all the buttons

--- components/timer.js
+++ components/timer.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { useState } from 'react';
-import { Box, Button, Flex, Text, PlayIcon, IconButton } from 'native-base';
+import { Box, Flex, Text } from 'native-base';
 import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Timer = () => {
   const [seconds, setSeconds] = useState('00');
   const [miliSeconds, setMiliSeconds] = useState('00');
   const [isResumed, setIsResumed] = useState(false);
+  const [isRestartEnabled, setIsRestartEnabled] = useState(false);
+
+  // function for start button
+  const timerStartHandler = () => {
+    setIsResumed(true);
+    setIsRestartEnabled(true);
+  };
+
+  // function for restart button
+  const restartHandler = () => {
+    setIsResumed(false);
+  };
+
+  // function for pause button
+  const pauseHandler = () => {
+    setIsResumed(false);
+  };
+
+  // function for lap button
+  const lapHandler = () => {};
 
   return (
     <Flex
@@ -26,14 +46,23 @@ const Timer = () => {
       <Flex width="80%" flexDirection="row" justifyContent="space-between">
         {!isResumed && (
           <>
-            <AntDesign name="playcircleo" size={60} />
-            <MaterialCommunityIcons name="restart" size={60} color="black" />
+            <AntDesign
+              name="playcircleo"
+              size={60}
+              onPress={timerStartHandler}
+            />
+            <MaterialCommunityIcons
+              name="restart"
+              size={60}
+              onPress={restartHandler}
+              disabled={!isRestartEnabled}
+            />
           </>
         )}
         {isResumed && (
           <>
-            <AntDesign name="pausecircleo" size={60} />
-            <AntDesign name="flag" size={60} />
+            <AntDesign name="pausecircleo" size={60} onPress={pauseHandler} />
+            <AntDesign name="flag" size={60} onPress={lapHandler} />
           </>
         )}
       </Flex>
